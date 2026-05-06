0f5d99e93900cba5b420d17e4b64913205f815f4
Your Name <your_email@example.com>
2026-05-06 17:34:29 +0530

    even num's btw 1-10

---
+++ EvenNum1.java
@@ -0,0 +1,16 @@
+class EvenNum1{
+    public static void main(String[]args)
+    {
+        for(int i=1;i<=10;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i+" is a Even number");
+            }
+            else
+            {
+                System.out.println(i+" is not an even number");
+            }
+        }
+    }
+}
