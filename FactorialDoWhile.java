2ef9c36c4ee046bb32c87909b73bb94e01241244
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-10 12:03:20 +0530

    factorial-do-while

---
+++ FactorialDoWhile.java
@@ -0,0 +1,15 @@
+class FactorialDoWhile{
+    public static void main(String[]args)
+    {
+        int num=5;
+        int fact=1;
+        int i=1;
+
+        do
+        {
+            fact=fact*i;
+            i++;
+        }while(i<=num);
+        System.out.println("The factorial of number is "+fact);
+    }
+}
