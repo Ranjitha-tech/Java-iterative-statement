68902a771da0572f2958576e56c4e7c9e682d145
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-10 12:09:04 +0530

    7-tables

---
+++ Multipli.java
@@ -0,0 +1,15 @@
+class Multipli{
+    public static void main(String[]args)
+    {
+        int num=7;
+        int i=1;
+        int limit=10;
+          
+        System.out.println("Multiply of 7 is "+num);
+        do
+        {
+            System.out.println(num*i);
+            i++;
+        }while(i<=limit);
+    }
+}
