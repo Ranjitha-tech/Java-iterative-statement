199e172173d634936034e1ad855d5c617f21a280
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-10 11:37:59 +0530

    name-printing-repeatedly

---
+++ MyName.java
@@ -0,0 +1,10 @@
+class MyName{
+    public static void main(String[]args)
+    {
+        String name="Ranjitha";
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println(name);
+        }
+    }
+}
