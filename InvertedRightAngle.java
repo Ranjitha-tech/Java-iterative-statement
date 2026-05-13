2607dd506205f8c683b7e4a564f3a92548ca81f1
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-13 10:41:14 +0530

    PatternMatching2

---
+++ InvertedRightAngle.java
@@ -0,0 +1,14 @@
+class InvertedRightAngle{
+    public static void main(String[]args)
+    {
+        int i,j,n=5;
+        for(i=5;i>=1;i--)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
