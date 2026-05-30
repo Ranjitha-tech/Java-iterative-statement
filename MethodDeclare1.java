887b35f7c65a1850aa624196106f1d1b3960d449
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-30 19:09:47 +0530

    w/o-arg-w/o-Rt

---
+++ MethodDeclare1.java
@@ -0,0 +1,13 @@
+public class MethodDeclare1{
+    void display()
+        {
+            System.out.println("Hello Manu");
+        }
+        public static void main(String[] args) {
+            System.out.println("Start");
+            MethodDeclare1 obj=new MethodDeclare1();
+            obj.display();
+            System.out.println("End");
+        }
+    
+}
