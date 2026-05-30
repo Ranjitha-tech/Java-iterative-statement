9d3ee7d861ea399cc31cb585086d12568d519dcf
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-05-30 18:58:06 +0530

    Default-value-of-all-dt

---
+++ DefaultValueDemo.java
@@ -0,0 +1,25 @@
+public class DefaultValueDemo{
+    byte a;
+        short b;
+        int c;
+        long d;
+        float e;
+        double f;
+        char g;
+        boolean h;
+        String i;
+    public static void main(String[] args) {
+        
+        
+        DefaultValueDemo d = new DefaultValueDemo();
+        System.out.println(d.a);
+        System.out.println(d.b);
+        System.out.println(d.c);
+        System.out.println(d.d);
+        System.out.println(d.e);
+        System.out.println(d.f);
+        System.out.println(d.g);
+        System.out.println(d.h);
+        System.out.println(d.i);
+    }
+}
