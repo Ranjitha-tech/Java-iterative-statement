70dfc27ebecd77a32ee96dc22aed9f38bc204963
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-06-01 18:50:02 +0530

    Gmail-login

---
+++ Gmail.java
@@ -0,0 +1,18 @@
+public class Gmail{
+    public static void main(String[]args)
+    {
+        String user_id ="ranjitha@gmail.com";
+        int password = 12345;
+        if(user_id == "ranjitha@gmail.com")
+        {
+            if(password==12345)
+        {
+            System.out.println("Login Successful");
+        }
+        else{
+            System.out.println("Incorrect Password");
+        }
+    }
+}
+
+}
