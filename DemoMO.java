249024e0e96d4b98031ed327e422f0dcf1b5c012
Ranjitha-tech <ranjithanagaraj5642@gmail.com>
2026-06-01 19:12:44 +0530

    Method-Overloading-Demo

---
+++ DemoMO.java
@@ -0,0 +1,16 @@
+public class DemoMO{
+    public static void main(String[] args) {
+        System.out.println("Start");
+        System.out.println("Hello java");
+        main(10);
+        main(10.45);
+        System.out.println("Stop");
+    }
+    public static void main(int a){
+        System.out.println("a: "+a);
+    }
+     public static void main(double b){
+        System.out.println("b: "+b);
+}
+}
+
